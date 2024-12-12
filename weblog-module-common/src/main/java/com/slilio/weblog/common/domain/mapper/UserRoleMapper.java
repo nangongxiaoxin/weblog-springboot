25fd648c5f4aed034808b2823fb1491db322f780
nangongxiaoxin <bx_tianhuan@163.com>
2024-12-13 00:01:31 +0800

    202412130001

--- weblog-module-common/src/main/java/com/slilio/weblog/common/domain/mapper/UserRoleMapper.java
+++ weblog-module-common/src/main/java/com/slilio/weblog/common/domain/mapper/UserRoleMapper.java
@@ -1,9 +1,12 @@
 package com.slilio.weblog.common.domain.mapper;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.slilio.weblog.common.domain.dos.UserDO;
 import com.slilio.weblog.common.domain.dos.UserRoleDO;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface UserRoleMapper extends BaseMapper<UserRoleDO> {
@@ -19,5 +22,22 @@ public interface UserRoleMapper extends BaseMapper<UserRoleDO> {
         wrapper.eq(UserRoleDO::getUsername,username);
 
         return selectList(wrapper);
+    }
+
+    /**
+     * 根据用户名修改密码
+     * @param username
+     * @param password
+     * @return
+     */
+    default int updatePasswordByUsername(String username, String password){
+        LambdaUpdateWrapper<UserDO> wrapper = new LambdaUpdateWrapper<>();
+//        设置需要更新的字段
+        wrapper.set(UserDO::getPassword, password);
+        wrapper.set(UserDO::getUpdateTime, LocalDateTime.now());
+//        更新条件
+        wrapper.eq(UserDO::getUsername, username);
+
+        return update(null, wrapper);
     }
 }
