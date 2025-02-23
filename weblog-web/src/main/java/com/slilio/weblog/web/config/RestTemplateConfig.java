95b9d6edacd64f413c6d6b8c261bc8c162922487
nangongxiaoxin <bx_tianhuan@163.com>
2025-02-24 01:15:56 +0800

    202502240115

--- weblog-web/src/main/java/com/slilio/weblog/web/config/RestTemplateConfig.java
+++ weblog-web/src/main/java/com/slilio/weblog/web/config/RestTemplateConfig.java
@@ -11,8 +11,8 @@ public class RestTemplateConfig {
   @Bean
   public RestTemplate restTemplate() {
     SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
-    factory.setConnectTimeout(5000); // 链接超时时间为5s
-    factory.setReadTimeout(5000); // 读取超时时间为5s
+    factory.setConnectTimeout(6000); // 链接超时时间为5s
+    factory.setReadTimeout(6000); // 读取超时时间为5s
     return new RestTemplate(factory);
   }
 }
